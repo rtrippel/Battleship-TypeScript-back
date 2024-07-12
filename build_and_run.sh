96af59a5a614edf83e1a1c21e176f9e8b727cc9e
Roman Trippel <rtrippel84@gmail.com>
2024-07-12 10:39:17 +0200

    Added https and wss

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+# Check if Docker Desktop is running
+if ! docker info >/dev/null 2>&1; then
+    # Docker Desktop is not running, attempt to start it
+    open --background -a Docker
+    echo "Starting Docker Desktop..."
+    # Wait for Docker to start (adjust sleep time as needed)
+    sleep 5
+fi
+
+# Continue with your deployment script here
+
 # Load environment variables from deploy.env
 set -o allexport
 source .env
 set +o allexport
+
+# Remove old files on the remote server and local
+rm ./my-app.tar
+rm -r ./dist
+
+ssh -i "$SSH_KEY" "$REMOTE_USER@$SERVER_IP" << EOF
+docker-compose --env-file $REMOTE_PATH/.env -f $REMOTE_PATH/docker-compose.yml down
+docker image rm my-app:latest || true
+EOF
+
+ssh -i "$SSH_KEY" "$REMOTE_USER@$SERVER_IP" "rm -r $REMOTE_PATH"
 
 # Install dependencies
 npm install
@@ -11,32 +33,34 @@ npm install
 # Build the project
 npm run build
 
-# Build the Docker image for amd64
+Build the Docker image for amd64
+
 docker build --platform linux/amd64 -t my-app .
 
 # Save the Docker image to a file
 docker save -o my-app.tar my-app:latest
 
-# Wait until the Docker image file is completely created
-echo "Waiting for the Docker image file to be created..."
-sleep 10
+## Wait until the Docker image file is completely created
+#echo "Waiting for the Docker image file to be created..."
+#sleep 10
 
+# Create the directory if it doesn't exist
 ssh -i "$SSH_KEY" "$REMOTE_USER@$SERVER_IP" "mkdir -p $REMOTE_PATH"
 
-echo "Trying to COPY...my-app.tar"
 # Copy the Docker image file to the remote server
 scp -i "$SSH_KEY" my-app.tar "$REMOTE_USER@$SERVER_IP:$REMOTE_PATH/my-app.tar"
 
-echo "Trying to COPY...$DOCKER_COMPOSE_FILE"
 # Copy the Docker Compose file to the remote server
 scp -i "$SSH_KEY" "$DOCKER_COMPOSE_FILE" "$REMOTE_USER@$SERVER_IP:$REMOTE_PATH/docker-compose.yml"
 
-echo "Trying to COPY...$ENV_FILE"
 # Copy the .env file to the remote server
 scp -i "$SSH_KEY" "$ENV_FILE" "$REMOTE_USER@$SERVER_IP:$REMOTE_PATH/.env"
+
+echo "Finished Copy Files..."
 
 # SSH into the remote server, load the Docker image, and run Docker Compose
 ssh -i "$SSH_KEY" "$REMOTE_USER@$SERVER_IP" << EOF
 docker load -i $REMOTE_PATH/my-app.tar
-docker-compose --env-file .env -f $REMOTE_PATH/docker-compose.yml up -d
+docker-compose --env-file $REMOTE_PATH/.env -f $REMOTE_PATH/docker-compose.yml up
+#docker-compose --env-file $REMOTE_PATH/.env -f $REMOTE_PATH/docker-compose.yml up -d
 EOF
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,49 @@
 import "reflect-metadata";
 import express from "express";
-import { createServer } from "http";
-import { Server } from "ws";
+import https from "https";
+import { Server, WebSocket } from "ws";
+import cors from "cors";
 import { AppDataSource } from "./data-source";
 import messageRoutes from "./routes/messageRoutes";
 import { Message } from "./entities/Message";
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
+
+const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+const certificatePath = process.env.CERTIFICATE_PATH;
+
+if (!privateKeyPath || !certificatePath) {
+    console.error('Failed to find environment variables PRIVATE_KEY_PATH or CERTIFICATE_PATH.');
+    process.exit(1);
+}
+
+let privateKey;
+let certificate;
+
+try {
+    privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+    certificate = fs.readFileSync(certificatePath, 'utf8');
+} catch (err) {
+    if (err instanceof Error) {
+        console.error('Error reading key or certificate:', err.message);
+    } else {
+        console.error('Unknown error:', err);
+    }
+    process.exit(1);
+}
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+};
 
 const app = express();
-const server = createServer(app);
+const server = https.createServer(credentials, app);
 const wss = new Server({ server });
 
+app.use(cors());
 app.use(express.json());
 app.use("/", messageRoutes);
 
@@ -18,7 +52,13 @@ wss.on("connection", (ws) => {
         const messageRepository = AppDataSource.getRepository(Message);
         const newMessage = messageRepository.create({ content: message.toString() });
         await messageRepository.save(newMessage);
-        ws.send(`Message saved: ${newMessage.content}`);
+
+        const newMessageData = JSON.stringify(newMessage);
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(newMessageData);
+            }
+        });
     });
 
     ws.send("Connected to WebSocket server");
